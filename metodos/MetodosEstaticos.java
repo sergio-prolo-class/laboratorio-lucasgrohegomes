2a74f00917cb43f37414542063d4303d5ef58ebf
Lucas Grohe Gomes <lucas_g_gomes@estudante.sesisenai.org.br>
2025-10-28 22:05:27 +0000

    Metodos

---
+++ metodos/MetodosEstaticos.java
@@ -0,0 +1,11 @@
+package metodos;
+
+public class MetodosEstaticos {
+    public static void main() {
+        somarNumeros(1, 2);
+    }
+
+    public static int somarNumeros(int x, int y) {
+        return x + y;
+    }
+}
